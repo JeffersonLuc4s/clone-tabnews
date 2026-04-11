9bb1fd7dbf5cbcb03fe1bfaa37d60172f7d2f143
Jefferson Lucas <103851320+JeffersonLuc4s@users.noreply.github.com>
2026-04-11 23:11:05 +0000

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -14,7 +14,7 @@ async function status(request, response) {
 
   const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1 AND state = 'active'",
     values: [databaseName],
   });
   const databaseOpenedConnectionsValue =
